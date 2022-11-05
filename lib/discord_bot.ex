6fe698ec769839ba149d11dfa494b0f086e58fbc
Trey Pendragon <tpendragon@princeton.edu>
2022-11-05 16:45:44 -0700

    Initial generation.

---
+++ lib/discord_bot.ex
@@ -0,0 +1,18 @@
+defmodule DiscordBot do
+  @moduledoc """
+  Documentation for `DiscordBot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DiscordBot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
