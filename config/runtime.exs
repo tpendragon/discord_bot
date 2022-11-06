5996f65efd80b46bfc681a22ae22a08243018e9d
Trey Pendragon <tpendragon@princeton.edu>
2022-11-05 19:15:02 -0700

    Make config work.

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,2 +1,9 @@
 import Config
-Dotenv.load!
+if config_env() == :dev || config_env() == :test do
+  Dotenv.load!
+end
+
+# These are accessible in code like this:
+# Application.fetch_env!(:discord_bot, :bot_token)
+config :discord_bot,
+  bot_token: System.get_env("BOT_TOKEN")
